afdac299c4a6a42df855d3858c4c7e82e1b47d8a
abdulalimsujon <alimsujon12@gmail.com>
2025-10-05 13:42:00 +0600

    kruskal algorithm with dsu

--- kruskal_algo.cpp
+++ kruskal_algo.cpp
@@ -1,69 +1,80 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Edge{
+
+// Edge class to represent a graph edge
+class Edge {
+public:
     int a, b, c;
-    public:
-    Edge (int a ,int b,int c){
+    Edge(int a, int b, int c) {
         this->a = a;
         this->b = b;
         this->c = c;
     }
 };
 
-int par[1005];
-int gSize[1005];
-int find(int node)
-{
-    if(par[node]== -1){
-        return node;
-    }
-    int leader = find(par[node]);
-    par[node] = leader;
-    return leader;
+// Comparator function to sort edges by cost
+bool cmp(Edge l, Edge r) {
+    return l.c < r.c;
 }
 
-void dsu_union(int node1,int node2){
+int par[1005];     // Parent array for DSU
+int gSize[1005];   // Group size array for DSU
 
+// DSU find function with path compression
+int find(int node) {
+    if (par[node] == -1) return node;
+    return par[node] = find(par[node]);
+}
+
+// DSU union by size
+void dsu_union(int node1, int node2) {
     int leader1 = find(node1);
     int leader2 = find(node2);
-    if(gSize[leader1] > gSize[leader2]){
-        par[leader2] = leader1;
-        gSize[leader1] += gSize[leader2];
-    }else{
-         par[leader1] = leader2;
-        gSize[leader2] += gSize[leader1];  
+
+    if (leader1 != leader2) {
+        if (gSize[leader1] > gSize[leader2]) {
+            par[leader2] = leader1;
+            gSize[leader1] += gSize[leader2];
+        } else {
+            par[leader1] = leader2;
+            gSize[leader2] += gSize[leader1];
+        }
     }
 }
-int main()
-{
-       memset(par, -1, sizeof(par));
-       memset(gSize, 1, sizeof(gSize));
-       int n, e;
-       int totalCost = 0;
-       cin >> n >> e;
-       vector<Edge> edge_list;
-       while (e--)
-       {
-           int a, b, c;
-           cin >> a >> b >> c;
-           edge_list.push_back(Edge(a, b, c));
-       };
-       bool cmp(Edge l, Edge r)
-       {
-           return l.c < r.c;
-       };
-       sort(edge_list.begin(), edge_list.end(), cmp);
 
-       for (auto ed : edge_list)
-       {
-           int parA = find(ed.a);
-           int parB = find(ed.b);
-           if(parA != parB){
-               dsu_union(ed.a, ed.b);
-               totalCost += ed.c;
-           }
-       }
+int main() {
+    int n, e;
+    cin >> n >> e;
 
-       cout << totalCost << "";
-       return 0;
+    // Initialize DSU
+    memset(par, -1, sizeof(par));
+    for (int i = 0; i <= n; i++) gSize[i] = 1;
+
+    vector<Edge> edge_list;
+
+    // Input edges
+    for (int i = 0; i < e; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edge_list.push_back(Edge(a, b, c));
+    }
+
+    // Sort edges by cost
+    sort(edge_list.begin(), edge_list.end(), cmp);
+
+    int totalCost = 0;
+
+    // Kruskal's Algorithm
+    for (auto ed : edge_list) {
+        int parA = find(ed.a);
+        int parB = find(ed.b);
+        if (parA != parB) {
+            dsu_union(ed.a, ed.b);
+            totalCost += ed.c;
+        }
+    }
+
+    cout << totalCost << endl;
+
+    return 0;
 }
